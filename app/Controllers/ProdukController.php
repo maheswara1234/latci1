2fe293c263ba491440b6b72c8d3bbb6a1854b713
maheswara1234 <mahesyihab2015@gmail.com>
2026-06-10 18:45:35 +0700

    memeperbaiki error dan membuat halaman keranjang

--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -6,6 +6,7 @@ use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
 use App\Models\ProductModel;
+use Dompdf\Dompdf;
 
 class ProdukController extends BaseController
 {
@@ -13,6 +14,7 @@ class ProdukController extends BaseController
 
     function __construct()
     {
+        helper('form');
         $this->productModel = new ProductModel();
     }
     public function index()
@@ -80,5 +82,36 @@ class ProdukController extends BaseController
         $this->productModel->delete($id);
 
         return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+    }
+
+    public function download()
+    {
+        // Ambil data produk dari database
+        $products = $this->productModel->findAll();
+
+        // Render view menjadi HTML
+        $html = view('produk/download_pdf', [
+            'products' => $products
+        ]);
+
+        // Nama file PDF
+        $filename = date('Y-m-d-H-i-s') . '-produk.pdf';
+
+        // Inisialisasi Dompdf
+        $dompdf = new Dompdf();
+
+        // Load HTML ke Dompdf
+        $dompdf->loadHtml($html);
+
+        // Setting ukuran kertas dan orientasi
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Generate PDF
+        $dompdf->render();
+
+        // Download / tampilkan PDF
+        $dompdf->stream($filename, [
+            'Attachment' => true
+        ]);
     }
 }
